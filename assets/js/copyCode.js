9597cb6e99c43593fff15731b40ebff83ebb598c
Jun Xu <jun.xu@tonoinfo.com>
2022-11-15 10:18:20 +0800

    Add "copy button" for code.

---
+++ assets/js/copyCode.js
@@ -0,0 +1,20 @@
+document
+    .querySelector('.page-content')
+    .querySelectorAll('div.highlight')
+    .forEach(function (pre) {
+      var button = document.createElement('button');
+      var copyText = 'COPY';
+      button.className = 'copy';
+      button.type = 'button';
+      button.ariaLabel = 'Copy code to clipboard';
+      button.innerText = copyText;
+      button.addEventListener('click', function () {
+        var code = pre.querySelector('code').innerText.trim();
+        navigator.clipboard.writeText(code);
+        button.innerText = 'COPIED';
+        setTimeout(function () {
+          button.innerText = copyText;
+        }, 1500);
+      });
+      pre.appendChild(button);
+    });
